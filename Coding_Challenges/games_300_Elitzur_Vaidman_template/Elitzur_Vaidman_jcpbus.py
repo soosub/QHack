2da7bc53eccd280d868e2a94e0ae88fc985fd9d1
jozbus <jozbus@ethz.ch>
2022-02-22 17:42:57 +0100

    start elitzur-vaidman

---
+++ Coding_Challenges/games_300_Elitzur_Vaidman_template/Elitzur_Vaidman_jcpbus.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python3
+
+import sys
+import pennylane as qml
+from pennylane import numpy as np
+
+dev = qml.device("default.qubit", wires=1, shots=1)
+
+
+@qml.qnode(dev)
+def is_bomb(angle):
+    """Construct a circuit at implements a one shot measurement at the bomb.
+
+    Args:
+        - angle (float): transmissivity of the Beam splitter, corresponding
+        to a rotation around the Y axis.
+
+    Returns:
+        - (np.ndarray): a length-1 array representing result of the one-shot measurement
+    """
+
+    # QHACK #
+    qml.RY(angle, wires=0)
+    # QHACK #
+
+    return qml.sample(qml.PauliZ(0))
+
+
+@qml.qnode(dev)
+def bomb_tester(angle):
+    """Construct a circuit that implements a final one-shot measurement, given that the bomb does not explode
+
+    Args:
+        - angle (float): transmissivity of the Beam splitter right before the final detectors
+
+    Returns:
+        - (np.ndarray): a length-1 array representing result of the one-shot measurement
+    """
+
+    # QHACK #
+    qml.RY(angle, wires=0)
+    qml.RY(angle, wires=0)
+    # QHACK #
+
+    return qml.sample(qml.PauliZ(0))
+
+
+def simulate(angle, n):
+    """Concatenate n bomb circuits and a final measurement, and return the results of 10000 one-shot measurements
+
+    Args:
+        - angle (float): transmissivity of all the beam splitters, taken to be identical.
+        - n (int): number of bomb circuits concatenated
+
+    Returns:
+        - (float): number of bombs successfully tested / number of bombs that didn't explode.
+    """
+
+    bombs_no_explosion = 0
+
+    # QHACK #
+    for i in range(n):
+        bombs_no_explosion += (1 - bomb_tester(angle)) / 2
+    bombs_no_explosion += bomb_tester(angle)
+
+    return n / bombs_no_explosion
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    output = simulate(float(inputs[0]), int(inputs[1]))
+    print(f"{output}")
